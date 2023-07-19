1d086f25bd8d064ca7f65f0cbe7332c777ac3c16
Bhupendra Singh Rawat <bhupendrasr08@gmail.com>
2023-07-19 20:32:03 +0530

    tabs not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ModalService } from './services/modal.service'; 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clips';
+  
+  
+  ngOnInit(){
+     
+  }
 }
