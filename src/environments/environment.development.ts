7c03ad9dec143c7c458569d7386b42f943ccb9a8
Bhupendra Singh Rawat <bhupendrasr08@gmail.com>
2023-07-27 20:48:03 +0530

    some things

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production:true,
+    firebase : {
+        apiKey: "AIzaSyCJHQ4AOXY9CFNEhvMKcrryxxKaHHdanI4",
+        authDomain: "clippy-50546.firebaseapp.com",
+        projectId: "clippy-50546",
+        storageBucket: "clippy-50546.appspot.com",
+        messagingSenderId: "95415783242", // wont throw error if unavailable
+        appId: "1:95415783242:web:0d66f622d1e148edfffbb3"
+      }
+
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+
+    firebase : {
+        apiKey: "AIzaSyCJHQ4AOXY9CFNEhvMKcrryxxKaHHdanI4",
+        authDomain: "clippy-50546.firebaseapp.com",
+        projectId: "clippy-50546",
+        storageBucket: "clippy-50546.appspot.com",
+        messagingSenderId: "95415783242",
+        appId: "1:95415783242:web:0d66f622d1e148edfffbb3"
+      }
+
+
+};
